1c83d94a4cca849f37bb41a838cbe14a1d34e594
Cristian Martinez <cristianfmm121134@gmail.com>
2025-08-25 13:17:38 -0500

    Versión 1.1

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
- var host ="http://localhost:8000/";
-
+var host ="http://localhost:8000/";
+//Prueba 1 Cristian
 function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
